96ad6413d994a3b3aad23e8a45dc95bf31876925
parisafz <pari.fz2024@gmail.com>
2024-09-30 15:52:23 +0330

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/insert/post', [PostController::class, 'index']);
+
+Route::get('/insert/user', [UserController::class, 'index']);
