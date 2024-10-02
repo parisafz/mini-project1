c6fda8d5f351f427a12b02823cb1bc15e317c2f8
parisafz <pari.fz2024@gmail.com>
2024-10-02 13:08:30 +0330

    show,edit and delete posts feature added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +8,3 @@ Route::get('/', function () {
 });
 
 Route::resource('post', PostController::class);
-
-
-Route::get('user/create', [UserController::class, 'index']);
